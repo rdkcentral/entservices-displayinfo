f34516748c4f4e09f1dd244655ae11002d04f6be
srinibas15 <113517102+srinibas15@users.noreply.github.com>
2026-06-15 17:07:03 +0530

    Update DisplayInfo.cpp

--- plugin/DisplayInfo.cpp
+++ plugin/DisplayInfo.cpp
@@ -137,8 +137,6 @@ namespace Plugin {
 
             // Stop processing:
             RPC::IRemoteConnection* connection = service->RemoteConnection(_connectionId);
-             //Added for Crash
-            _connectionProperties = nullptr;
             VARIABLE_IS_NOT_USED uint32_t result = _connectionProperties->Release();
             _connectionProperties = nullptr;
 
