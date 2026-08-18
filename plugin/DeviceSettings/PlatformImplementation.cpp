364c65e47b185993c7f9271e515245453022fe2b
Arun Madhavan <arun_madhavan@comcast.com>
2026-08-18 15:58:08 -0400

    RDKEMW-23533,REFPLTV-3210: add missing SDR into stbcapabilities but consider as HDR_OFF

--- plugin/DeviceSettings/PlatformImplementation.cpp
+++ plugin/DeviceSettings/PlatformImplementation.cpp
@@ -879,8 +879,9 @@ public:
         if(capabilities & dsHDRSTANDARD_HLG) hdrCapabilities.push_back(HDR_HLG);
         if(capabilities & dsHDRSTANDARD_DolbyVision) hdrCapabilities.push_back(HDR_DOLBYVISION);
         if(capabilities & dsHDRSTANDARD_TechnicolorPrime) hdrCapabilities.push_back(HDR_TECHNICOLOR);
-        if(capabilities & dsHDRSTANDARD_Invalid)hdrCapabilities.push_back(HDR_OFF);
-
+	// Treat SDR and Invalid as HDR_OFF to ensure that the list always has at least one valid entry
+        if(capabilities & dsHDRSTANDARD_SDR) hdrCapabilities.push_back(HDR_OFF);
+        if(capabilities & dsHDRSTANDARD_Invalid) hdrCapabilities.push_back(HDR_OFF);
 
         type = Core::Service<HdrteratorImplementation>::Create<Exchange::IHDRProperties::IHDRIterator>(hdrCapabilities);
         return (type != nullptr ? Core::ERROR_NONE : Core::ERROR_GENERAL);
