10afcf1376e6f334bb94d34fb094ad355d354910
Manimaran Renganathan <manimaran_renganathan@comcast.com>
2026-09-16 10:52:27 +0000

    Resolved the Build entservices-deviceinfo workflow failure
    Signed-off-by: Manimaran Renganathan <manimaran_renganathan@comcast.com>

--- Tests/L1Tests/tests/DisplayInfoDeviceSettingsMock.h
+++ Tests/L1Tests/tests/DisplayInfoDeviceSettingsMock.h
@@ -141,8 +141,6 @@ public:
     MOCK_METHOD(Core::hresult, IsAudioMS12Decoded, (int32_t, bool&), (override));
     MOCK_METHOD(Core::hresult, GetAudioLEConfig, (int32_t, bool&), (override));
     MOCK_METHOD(Core::hresult, EnableAudioLEConfig, (int32_t, bool), (override));
-    MOCK_METHOD(Core::hresult, SetApplicationAudioConfig, (int32_t, const string&, bool), (override));
-    MOCK_METHOD(Core::hresult, GetApplicationAudioConfig, (int32_t, const string&, bool&), (override));
     MOCK_METHOD(Core::hresult, SetAudioDelay, (int32_t, uint32_t), (override));
     MOCK_METHOD(Core::hresult, GetAudioDelay, (int32_t, uint32_t&), (override));
     MOCK_METHOD(Core::hresult, SetAudioDelayOffset, (int32_t, uint32_t), (override));
